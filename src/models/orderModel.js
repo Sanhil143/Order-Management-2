300d15d517f00e94d3aacb598f441bfed9e844f5
Sanhil143 <sanhilrai143@gmail.com>
2024-02-10 00:08:08 +0530

    update

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,38 +1,40 @@
-
 module.exports = (sequelize, DataTypes) => {
-
-      const Orders = sequelize.define('orders', {
-            productName: {
-                  type: DataTypes.STRING,
-                  allowNull: false
-            },
-            productPrice: {
-                  type: DataTypes.INTEGER,
-                  allowNull: false
-            },
-            customer_id: {
-                  type: DataTypes.INTEGER,
-                  allowNull: false
-            },
-            customerName: {
-                  type: DataTypes.STRING,
-                  allowNull: false
-            },
-            quantity: {
-                  type: DataTypes.INTEGER,
-                  allowNull: false
-            },
-            orderStatus: {
-                  type: DataTypes.BOOLEAN,
-                  defaultValue: true
-            },
-            paymentStatus: {
-                  type: DataTypes.BOOLEAN,
-                  defaultValue: true
-            },
-      }, {
-            tableName: 'orders',
-            timestamps: true
-      })
-      return Orders
-}
+  const Orders = sequelize.define(
+    "orders",
+    {
+      productName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      productPrice: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      customer_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      customerName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      orderStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      paymentStatus: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      tableName: "orders",
+      timestamps: true,
+    }
+  );
+  return Orders;
+};
