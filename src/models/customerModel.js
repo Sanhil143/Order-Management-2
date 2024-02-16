108db2499addd9ef1a34cbabe183051995c7cfb3
Sanhil143 <sanhilrai143@gmail.com>
2024-02-17 00:05:44 +0530

    fix bug

--- src/models/customerModel.js
+++ src/models/customerModel.js
@@ -1,43 +1,46 @@
 module.exports = (sequelize, DataTypes) => {
+  const Customers = sequelize.define(
+    "customers",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
 
-      const Customers = sequelize.define('customers', {
-            name: {
-                  type: DataTypes.STRING,
-                  allowNull: false
-            },
-            
-            email: {
-                  type: DataTypes.STRING,
-                  unique: true,
-                  allowNull: false
-            },
-            password: {
-                  type: DataTypes.STRING,
-                  allowNull: false
-            },
-            productOrder:{
-                  type:DataTypes.INTEGER,
-                  defaultValue:0
-            },
-            category: {
-                  type: DataTypes.ENUM("Regular", "Gold", "Platinum"),
-                  defaultValue: "Regular"
-            },
-            balance: {
-                  type: DataTypes.INTEGER,
-                  defaultValue: 100
-            },
-            isDeleted:{
-                  type: DataTypes.BOOLEAN,
-                  defaultValue:false
-            },
-            deletedAt:{
-                  type:DataTypes.DATE,
-                  defaultValue:null
-            },
-      }, {
-            tableName: 'customers',
-            timestamps: true
-      })
-      return Customers;
-}
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      productOrder: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      category: {
+        type: DataTypes.ENUM("Regular", "Gold", "Platinum"),
+        defaultValue: "Regular",
+      },
+      balance: {
+        type: DataTypes.INTEGER,
+        defaultValue: 100,
+      },
+      isDeleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        defaultValue: null,
+      },
+    },
+    {
+      tableName: "customers",
+      timestamps: true,
+    }
+  );
+  return Customers;
+};
